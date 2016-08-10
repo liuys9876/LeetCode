0d4afaae964a3f01067babeb270216cbe83e0045
liuys9876 <liuys9876@163.com>
2016-08-10 14:46:31 +0800

    43

--- MultiplyStrings.java
+++ MultiplyStrings.java
@@ -1,11 +1,14 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MultiplyStrings {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String num1="63068080726329880611684115603801120940144776974937025638198670884099005492080675397052881650850279";
-		String num2="91851171339080006286661648785824201538663791893631297";
+		String num1="65787657567577777777776666666666666666666666666666666666666666666666";
+		String num2="5017988888888888888888888888888888888888888888888888888888888888889";
 		Solution43 solu = new Solution43();
 		long timestart=System.currentTimeMillis();
 		System.out.println(solu.multiply(num1, num2));
@@ -14,7 +17,42 @@ public class MultiplyStrings {
 	}
 
 }
+class Solution43 {
+    public String multiply(String num1, String num2) {
+	        String ans ="";
+	        int num1len = num1.length();
+	        int num2len = num2.length();
+	        if("0".equals(num1)||"0".equals(num2))
+	        {
+	        	ans="0";
+	        	return ans;
+	        }
+	        int[] arrPoint = new int[num1len+num2len+1];//临时记录每个数位位置的值,a*b的结果长度必然小于a与b的长度之和
+	        for(int i =0 ;i <num2len;i++)
+	        {
+	        	for(int j = 0;j<num1.length();j++)
+	        	{
+	        		arrPoint[i+j]+=Integer.parseInt(String.valueOf(num1.charAt(num1len-j-1)))*Integer.parseInt(String.valueOf(num2.charAt(num2len-i-1)));
+	        	}
+	        }
+	        
+	        for(int k=0;k<arrPoint.length;k++)
+	        {
+	        		ans=arrPoint[k]%10+ans;
+	        		if(k!=arrPoint.length-1)
+	        		arrPoint[k+1]+=arrPoint[k]/10;
+	        }
+	        while(ans.charAt(0)=='0')
+	        {
+	        	ans=ans.substring(1,ans.length());
+	        }
+	        
+	        return ans;
+    }
+}
 
+
+/*
 class Solution43 {
     public String multiply(String num1, String num2) {
         String ans ="";
@@ -91,3 +129,6 @@ class Solution43 {
     	return ans;
     }
 }
+
+
+*/
