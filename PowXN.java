ec9ee56f79d1ea29bc24a1aba0e7a37de760f111
liuys9876 <liuys9876@163.com>
2016-08-19 10:20:57 +0800

    50

--- PowXN.java
+++ PowXN.java
@@ -7,8 +7,8 @@ public class PowXN {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double x=8.003;
-		int n=100;
+		double x=-1.00000;
+		int n=-2147483648;
 		Solution50 solu = new Solution50();
 		long timestart = System.currentTimeMillis();
 		System.out.println(solu.myPow(x,n));
@@ -33,32 +33,46 @@ class Solution50 {
     		int[] arr ={};
     		double ans=1;
     		arr=div(Math.abs(n));
-    		int Pow = arr.length-1;
-    		for(int i = Pow ;i>0;i--)
+    		double temp=x;
+    		for(int i = 0;i<arr.length;i++)
     		{
-    			ans*=arr[i]*(1<<Pow);
-    			Pow--;
+    			if(arr[i]==0)
+    			{
+    				temp=temp*temp;
+    			}
+    			else
+    			{
+    				ans=ans*temp;
+    				temp=temp*temp;
+    			}
     		}
     		return ans;
     	}else
     	{
     		int[] arr ={};
     		double ans=1;
-    		arr=div(Math.abs(n));
-    		int Pow = arr.length-1;
-    		for(int i = Pow ;i>=0;i--)
+    		arr=div(Math.abs((long)n));
+    		double temp=x;
+    		for(int i = 0;i<arr.length;i++)
     		{
-    			ans*=arr[i]*(1<<Pow);
-    			Pow--;
+    			if(arr[i]==0)
+    			{
+    				temp=temp*temp;
+    			}
+    			else
+    			{
+    				ans=ans*temp;
+    				temp=temp*temp;
+    			}
     		}
     		return 1/ans;
     	}
     }
     //将n分解成2^i的相加的形式，n=l1*2^0+l2*2^1+……+ln*2^n;
-    private int[] div(int n)
+    private int[] div(long n)
     {
     	int MaxPow = 0;
-    	int tempN=n;
+    	long tempN=n;
     	while(n>1)
     	{
     		n=n>>1;
@@ -67,11 +81,12 @@ class Solution50 {
     	int[] arr = new int[MaxPow+1];
     	for(int j =MaxPow;j>=0 ;j--)
     	{
-    		if(tempN>(1<<j))
+    		//强转1为long类型，默认1为int类型，防止1<<31超过数位大小变为负值
+    		if(tempN>((long)1<<j))
     		{
     			arr[j]=1;
     			tempN-=(1<<j);
-    		}else if(tempN<(1<<j))
+    		}else if(tempN<((long)1<<j))
     		{
     			arr[j]=0;
     		}
